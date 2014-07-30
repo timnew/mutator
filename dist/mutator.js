4433ecdd4f5bd4d84ff52d1ced160bd4d3aad265
DD <timnew.wti@gmail.com>
2014-07-30 14:55:55 +0800

    Introduced fix to IE

--- dist/mutator.js
+++ dist/mutator.js
@@ -1,5 +1,5 @@
 (function() {
-  var mutate;
+  var fixForIE, mutate;
 
   mutate = function(instance, klass, args) {
     if (args == null) {
@@ -15,5 +15,28 @@
     klass.apply(instance, args);
     return instance;
   };
+
+  fixForIE = function(instance, klass, args) {
+    var k, v;
+    if (args == null) {
+      args = [];
+    }
+    if (typeof instance !== 'object') {
+      throw 'Instance must be an object';
+    }
+    if (typeof klass !== 'function') {
+      throw 'Klass must be a class';
+    }
+    for (k in klass) {
+      v = klass[k];
+      if (instance[k] === void 0) {
+        instance[k] = v;
+      }
+    }
+    klass.apply(instance, args);
+    return instance;
+  };
+
+  this.mutate = {}.__proto__ === void 0 ? (console.warn('__proto__ is not supported by current browser, fallback to hard-copy approach'), fixForIE) : mutate;
 
 }).call(this);
--- src/browser/mutator.coffee
+++ src/browser/mutator.coffee
@@ -5,3 +5,19 @@ mutate = (instance, klass, args = []) ->
   instance.__proto__ = klass.prototype
   klass.apply(instance, args)
   instance
+
+fixForIE = (instance, klass, args = []) ->
+  throw 'Instance must be an object' if typeof instance isnt 'object'
+  throw 'Klass must be a class' if typeof klass isnt 'function'
+  
+  for k,v of klass when instance[k] is undefined
+    instance[k] = v 
+
+  klass.apply(instance, args)
+  instance  
+
+@mutate = if ({}).__proto__ is undefined
+            console.warn '__proto__ is not supported by current browser, fallback to hard-copy approach'
+            fixForIE
+          else
+            mutate
